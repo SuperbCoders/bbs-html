6d71f485c648a485c0658024d5b2785af53b1c42
lexxbrose <lexxbrose@gmail.com>
2014-03-17 12:01:35 +0200

    blings11

--- blings/js/scroll.js
+++ blings/js/scroll.js
@@ -1,10 +1,16 @@
 $(function(){
-	var origOffsetY = $('.showcase').height()-390;
 	function scroll(){
 		if ($(window).scrollTop() >= 92) {$('.page-header-mid').addClass('nav-static');} 
 		else {$('.page-header-mid').removeClass('nav-static');}
-		if ($(window).scrollTop() >= origOffsetY) {$('.sidenav').addClass('sidenav-hidden');} 
-		else {$('.sidenav').removeClass('sidenav-hidden');}
 	}
 	document.onscroll = scroll;
+
+	if($('.sidenav').length){
+		var origOffsetY = $('.showcase').height()-165;
+		function scrollY(){
+			if ($(window).scrollTop() >= origOffsetY) {$('.sidenav').addClass('sidenav-hidden');} 
+			else {$('.sidenav').removeClass('sidenav-hidden');}
+		}
+		document.onscroll = scrollY;
+	};
 })
