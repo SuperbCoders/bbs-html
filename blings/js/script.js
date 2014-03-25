234912c2f2bbc29619160cb10c6b156f1543e625
lexxbrose <lexxbrose@gmail.com>
2014-03-25 11:12:39 +0200

    blings 13

--- blings/js/script.js
+++ blings/js/script.js
@@ -6,7 +6,6 @@ $(function(){
 		if($('#carousel4').length){$('#carousel4').carouFredSel({auto:false,responsive:true,width:'100%',height:'verible',scroll:4,prev:'#prev4',next:'#next4',items: {width:240,height:'verible',visible:{min:2,max:4}},swipe:{onMouse:true,onTouch:true}}).trigger('resize');};
 		if($('#carousel5').length){$('#carousel5').carouFredSel({auto:true,responsive:true,width:'100%',height:'verible',scroll:{items:2,duration:1000},pagination:'#pager5',items: {height:'verible',visible:{min:1,max:2}},swipe:{onMouse:true,onTouch:true}}).trigger('resize');};
 		if($('#carousel6').length){$('#carousel6').carouFredSel({auto:false,responsive:false,width:230,height:100,scroll:1,pagination:'#pager6',swipe:{onMouse:true,onTouch:true}});};
-		if($('#carousel7').length){$('#carousel7').carouFredSel({circular:0,auto:false,responsive:true,width:'100%',height:'verible',scroll:1,prev:'#prev7',next:'#next7',items: {height: '100%',visible:{min:1,max:3},start:-1},swipe:{onMouse:true,onTouch:true}}).trigger('resize');};
 		if($('#grid').length){$('#grid').masonry({itemSelector:'.item',isFitWidth:true});};
 	});
 	if($('.respotext').length){$('.respotext').responsiveText();};
@@ -19,4 +18,8 @@ $(function(){
 	$('.search input').focusout(function(){$(this).parents('.search').removeClass('focus');});
 	$('.btn-navbar').click(function(){$('.page-header-mid').addClass('nav-fullsize');$('.wrapper-dropdown').removeClass('active');$('body').addClass('overley');return false;});
 	$('.page-header-mid .close').click(function(){$('.page-header-mid').removeClass('nav-fullsize');$('body').removeClass('overley');return false;});
+	
+	$('.product .order .btn').click(function(){
+		$(this).animate({opacity:0},1000,function(){$(this).html('Спасибо');}).animate({opacity: 1},1000,function(){$(this).animate({opacity:0},1000,function(){$(this).html('В корзину');}).animate({opacity: 1},1000);});
+	});
 });
